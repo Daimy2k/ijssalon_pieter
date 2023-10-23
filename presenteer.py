bd4b60071985590dca2d5fa3c2212a5476c620d9
Daimy <daimy1409@gmail.com>
2023-10-23 19:25:26 +0200

    presenteer.py toegevoegd

---
+++ presenteer.py
@@ -0,0 +1,10 @@
+def presenteer(dictionary,totaal):
+    
+    uitvoer=[]
+    for k,v in dictionary.items():
+        print(k,":",v, "euro")
+    print("==========================")
+    print(f"totaal : {totaal} euro")
+    return uitvoer
+
+print(presenteer)
